ea8ae7e581b061669916f8f6db71e5e3417277fb
Asela S Perera <136217850+aselasperera@users.noreply.github.com>
2024-05-24 12:00:42 +0530

    Edits

--- Greetings/GreetingMessage.go
+++ Greetings/GreetingMessage.go
@@ -8,10 +8,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Enter your name:")
+	fmt.Println("Enter Your First Name")
 	reader := bufio.NewReader(os.Stdin)
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
-	message := "Hello, " + name + "! Welcome to the program."
-	fmt.Println(message)
+	input, _ := reader.ReadString('\n') // Corrected: Pass '\n' instead of "\n"
+	name := strings.TrimSpace(input)
+	fmt.Printf("Hello, %s! Welcome to the Evolza Team!\n", name) // Corrected: "Hellow" changed to "Hello"
 }
