64222c3ec3dfb1c76db16545fb9e09de7e00a0a1
Asela S Perera <136217850+aselasperera@users.noreply.github.com>
2024-05-25 11:39:22 +0530

    Edits

--- GuessingGame/Game.go
+++ GuessingGame/Game.go
@@ -7,26 +7,21 @@ import (
 )
 
 func main() {
-	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
-
-	// Generate a random number between 1 and 100
 	target := rand.Intn(100) + 1
 
-	// Initialize the guess variable
 	var guess int
 
-	fmt.Println("Welcome to the Guessing Game!")
+	fmt.Println("Welcome to the Evolza Guessing Game!")
 	fmt.Println("I have selected a random number between 1 and 100.")
 	fmt.Println("Can you guess what it is?")
+	fmt.Println("Let's get started!")
 
-	// Loop until the user guesses the correct number
 	for {
-		// Prompt the user for a guess
+
 		fmt.Print("Enter your guess: ")
 		fmt.Scanln(&guess)
 
-		// Check the user's guess
 		if guess < target {
 			fmt.Println("Too low! Try again.")
 		} else if guess > target {
